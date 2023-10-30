554a1a0144e5eb10ae8d77c08b5c0c4668d14f84
ahmadzafar848 <ahmad.zafar848@gmail.com>
2023-10-30 17:15:28 +0500

    progressbar

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.example.webview
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     lateinit var geekWebView: WebView
+    lateinit var progressBar: ProgressBar
+    private var isPageLoading = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        progressBar=findViewById(R.id.progressbar)
          geekWebView=findViewById(R.id.mainwebview)
-        geekWebView.webViewClient= WebViewClient()
+
+        geekWebView.webViewClient=object: WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+               progressBar.visibility=View.VISIBLE
+                isPageLoading = true
+                disableUserInteraction()
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility=View.GONE
+                isPageLoading = false
+                enableUserInteraction()
+            }
+        }
+
+//        geekWebView.webViewClient= WebViewClient()
         geekWebView.loadUrl("https://www.geeksforgeeks.org/")
         geekWebView.settings.javaScriptEnabled = true
 
@@ -26,5 +48,16 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+
+    }
+    private fun disableUserInteraction() {
+        geekWebView.setOnTouchListener { _, _ -> true } // Disable touch events
+        geekWebView.isClickable = false
+        geekWebView.isFocusable = false
+    }
+    private fun enableUserInteraction() {
+        geekWebView.setOnTouchListener(null) // Enable touch events
+        geekWebView.isClickable = true
+        geekWebView.isFocusable = true
     }
 }
